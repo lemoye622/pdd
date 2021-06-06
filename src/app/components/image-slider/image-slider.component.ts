79264803ff75687d65b7f52f7e134f0816d8c9a5
lemoye622 <2557418823@qq.com>
2021-06-06 17:08:19 +0800

    feat: 完成轮播图组件

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 export interface ImageSlider {
   imgUrl: string;
@@ -11,10 +11,12 @@ export interface ImageSlider {
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss']
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() sliders: ImageSlider[] = [];
   @Input() sliderHeight = '100px';
   @Input() intervalBySeconds = 2;
+  selectedIndex = 0;
+  intervalId;
   // 在Augular8.0之后，@ViewChild要指定第二个参数
   // 表示该元素是在ngIf/ngFor包含之下，即动态，static就是false，否则为true（静态）
   @ViewChild('imageSlider', {static: true}) imgSlider: ElementRef;
@@ -39,16 +41,33 @@ export class ImageSliderComponent implements OnInit {
       this.rd2.setStyle(item.nativeElement, 'height', '100px');
     })
 
-    let i = 0;
-    setInterval(() => {
+    //  let i = 0;
+    this.intervalId = setInterval(() => {
       this.rd2.setProperty(
         this.imgSlider.nativeElement, 
         'scrollLeft', 
-        ((++i % this.sliders.length) * this.imgSlider.nativeElement.scrollWidth) / this.sliders.length);
+        (this.getIndex(++this.selectedIndex) * this.imgSlider.nativeElement.scrollWidth) / this.sliders.length);
     }, this.intervalBySeconds * 1000)
   }
 
-  // handleScroll(event) {
-  //   const ratio = event.target.scrollLeft * this.sliders.length /
-  // }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  // 处理数组越界
+  getIndex(idx: number): number {
+    return idx >= 0
+    ? idx % this.sliders.length
+    : this.sliders.length - (Math.abs(idx) % this.sliders.length);
+  }
+
+  handleScroll(event) {
+    // 滑动比例：往左滑动的 scrollLeft 乘以图片数组长度，再去除以整个的 scrollWidth
+    // 这个比例意即：现在滑动的到没到应该找的下一张，其实就是看每张图的宽度
+    // 如果超过了这一张图的宽度的一半了，那就进入下一张图片
+    const ratio = 
+    (event.target.scrollLeft * this.sliders.length) / event.target.scrollWidth;
+    // event.target.scrollLeft / (event.target.scrollWidth / this.sliders.length);
+    this.selectedIndex = Math.round(ratio);
+  }
 }
