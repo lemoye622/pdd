c414f0f944819f2701079b66ac175f4d83bb5e07
lemoye <2557418823@qq.com>
2021-12-11 16:46:50 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { Channel } from './shared/components';
 
 // interface TopMenu {
 //   title: string;
@@ -48,4 +49,19 @@ export class AppComponent {
   //   // console.log(index);
   //   this.selectedIndex = index;
   // }
+
+  channels: Channel[] = [
+    {
+      id: 1,
+      title: '限时秒杀',
+      icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5d4298059889417157e8492750328492.jpg?w=632&h=340',
+      link: 'hot'
+    },
+    {
+      id: 1,
+      title: '断码清仓',
+      icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e023dd94c146d0f27f7ae8e98213abff.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
+      link: 'men'
+    }
+  ]
 }
--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -15,20 +15,20 @@ export interface Channel {
   styleUrls: ['./horizontal-grid.component.css']
 })
 export class HorizontalGridComponent implements OnInit {
-  channels: Channel[] = [
-    {
-      id: 1,
-      title: '限时秒杀',
-      icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5d4298059889417157e8492750328492.jpg?w=632&h=340',
-      link: 'hot'
-    },
-    {
-      id: 1,
-      title: '断码清仓',
-      icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e023dd94c146d0f27f7ae8e98213abff.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
-      link: 'men'
-    }
-  ]
+  // channels: Channel[] = [
+  //   {
+  //     id: 1,
+  //     title: '限时秒杀',
+  //     icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5d4298059889417157e8492750328492.jpg?w=632&h=340',
+  //     link: 'hot'
+  //   },
+  //   {
+  //     id: 1,
+  //     title: '断码清仓',
+  //     icon: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/e023dd94c146d0f27f7ae8e98213abff.jpg?thumb=1&w=1533&h=575&f=webp&q=90',
+  //     link: 'men'
+  //   }
+  // ]
 
   // username = '';
 
